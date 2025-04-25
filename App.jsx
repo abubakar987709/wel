302b97585fb6f5fdfce024a01cfcc52776ae6253
abubakar987709 <abubakarshafique64@gmail.com>
2025-04-25 16:10:08 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useContext, useState } from 'react';
+import W1 from './components/W1';
+import Website1 from './components/Website1';
+import Index from './components';
+import End from './components/End';
+
+function App() {
+  return (
+    <>
+      <W1 />
+      <Website1 />
+      <Index />
+      <End />
+    </>
+  );
+}
+
+export default App;
